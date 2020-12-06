c350b10dc735641ca66ba3ced7dcd9daf0a8cb2d
CanvasDEV-br <75525719+CanvasDEV-br@users.noreply.github.com>
2020-12-06 15:09:09 -0300

    Project Update

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public float force;
+    public Transform spawnPoint;
+    public GameObject fishPrefab = null;
+    private GameObject temp;
+    private bool isIstantiate;
+
+    void Start()
+    {
+        temp = Instantiate(fishPrefab, spawnPoint);
+        temp.SendMessage("StartForce", force, SendMessageOptions.DontRequireReceiver);
+        isIstantiate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
