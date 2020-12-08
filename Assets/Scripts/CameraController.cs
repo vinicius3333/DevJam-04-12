004f95e01341690229c2c71a76bfce2c07f69ab1
Vinícius Martins <vinicius.martins@fcamara.com.br>
2020-12-07 23:30:55 -0300

    Adicionando efeito de camera e cenário do boss

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public static CameraController instance;
+
+    public GameObject[] cameras;
+    public float freezeTime = 1f;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    public void EnableCamera(GameObject camera) {
+        if (camera.activeInHierarchy) return;
+
+        for (int i = 0; i < cameras.Length; i++) {
+            cameras[i].SetActive(false);
+        }
+
+        camera.SetActive(true);
+
+        StartCoroutine(FreezingTime());
+    }
+
+    IEnumerator FreezingTime() {
+        Time.timeScale = 0;
+
+        yield return new WaitForSecondsRealtime(freezeTime);
+
+        Time.timeScale = 1;
+    }
+}
