2e5e1d8dcc658c4f45d8469e7d37ef47f4b1882a
CanvasDEV-br <75525719+CanvasDEV-br@users.noreply.github.com>
2020-12-08 00:52:53 -0300

    Adicao da foca

---
+++ Assets/Scripts/EnemyBallShot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBallShot : MonoBehaviour
+{
+    public float timeToDestroy; //tempo para destruir dps que bater em algo
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D col) 
+    {
+        Destroy(this.gameObject, timeToDestroy);
+    }
+        private void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Foca.cs
+++ Assets/Scripts/Foca.cs
@@ -3,16 +3,177 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Foca : MonoBehaviour
-{
+{  
+    public enum EnemyState
+    {
+        PATRULHANDO,
+        ATIRANDO
+    }
+
+    private PlayerController _PlayerController;
+    private Rigidbody2D enemyRb;
+    public EnemyState enemyCurrentState; //estado atual do inimigo
+    public GameObject shotPrefab;
+    public Transform shotPosition;
+    
+    public float timeInPoint; //tempo em cada ponto
+    public float enemySpeed;
+    public float enemyHP;
+
+    public float shotSpeed;
+    public float timeToShot;
+
+    public float maxDistance;
+
+    public Transform[] wayPoints; //pontons para percorrer, e tambem usado para definir a area que ele proteje
+    public GameObject head; //cabeça do inimigo e responsavel pelo dano do pulo
+
+    [Header("Controladores")]
+    private bool isLockPlayer;
+    private bool isShot;
+    private bool isPlayerDetected; //se o player entrou na area de colisao
+
+    private int idTarget = 0; //controla o objetivo
+    private bool isCenter = false; //controla se está centralizado no ponto objetivo
+    private Transform target; //objetivo
+    private Vector3 direction; //direcao do movimento setado da posAtual para o objetivp
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _PlayerController = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+
+        enemyRb = GetComponentInChildren<Rigidbody2D>();
+        target = wayPoints[idTarget];
+
+        direction = Vector3.Normalize(transform.position - target.position);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() {
+        //checa se o player está acima da cabeça
+        if(_PlayerController.gameObject.transform.position.y > head.transform.position.y && head.activeSelf == false)
+        {
+            head.SetActive(true);
+        }
+        else if(_PlayerController.gameObject.transform.position.y < head.transform.position.y && head.activeSelf == true)
+        {
+            head.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate() {
+
+        float distance = Vector3.Distance(transform.position, _PlayerController.transform.position);
+
+        if(distance > maxDistance)
+        {
+            enemyCurrentState = EnemyState.PATRULHANDO;
+        }
+        else if(distance <= maxDistance && isLockPlayer == false && enemyCurrentState == EnemyState.PATRULHANDO)
+        {
+            enemyCurrentState = EnemyState.ATIRANDO;
+        }
+
+
+        switch(enemyCurrentState)
+        {
+            case EnemyState.PATRULHANDO:
+                Patrulha();
+                StopCoroutine("ShotDelay");
+                isLockPlayer = false;
+                isShot = false;
+
+            break;
+
+            case EnemyState.ATIRANDO:
+                isLockPlayer = true;
+                enemyRb.velocity = Vector2.zero;
+                if(isShot == false)
+                {
+                    Atirar();
+                }
+                break;
+        }
+    }
+
+    private void OnBecameVisible() {
+        shotPosition.right = _PlayerController.transform.position - transform.position;
+    }
+
+     public void Patrulha()
     {
-        
+        //quando estiver centralizando
+        if(isCenter == false)
+        {
+            if(idTarget == 0 && transform.position.x <= wayPoints[0].transform.position.x)
+            {
+                idTarget = 1;
+                StartCoroutine("UpdateTarget");
+            }
+
+            if(idTarget == 1 && transform.position.x >= wayPoints[1].transform.position.x)
+            {   
+                idTarget = 0;
+                StartCoroutine("UpdateTarget");
+            }
+
+            enemyRb.velocity = new Vector2((enemySpeed * direction.x)*-1, enemyRb.velocity.y);
+        }
+        else
+        {
+            enemyRb.velocity = Vector2.zero;
+        }
+
+    }
+    void Atirar()
+    {   
+        shotPosition.right = _PlayerController.transform.position - transform.position;
+        if(isShot == false)
+        {
+            StartCoroutine("ShotDelay");
+        }
+    }
+
+    void Shot(){
+        isShot = true;
+        Rigidbody2D temp = Instantiate(shotPrefab, shotPosition.position, shotPosition.localRotation).GetComponent<Rigidbody2D>();
+
+        temp.transform.localRotation = shotPosition.localRotation;
+        temp.velocity = shotPosition.right * shotSpeed;
+
+    } //instancia o tiro
+
+    public void TakeHit() {
+        enemyHP--;
+
+        if (enemyHP <= 0) {
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    } //controle da vida
+
+     IEnumerator UpdateTarget() //muda para um novo ponto
+    {   
+        switch(enemyCurrentState)
+        {
+            case EnemyState.PATRULHANDO:
+                isCenter = true;
+                target = wayPoints[idTarget];
+
+                yield return new WaitForSeconds(timeInPoint);
+                direction = Vector3.Normalize(transform.position - target.position);
+                isCenter = false;
+            break;
+        }
+
+    }
+
+    IEnumerator ShotDelay()
+    {
+        Shot();
+        yield return new WaitForSeconds(timeToShot);
+        //StartCoroutine("ShotDelay");
+        isLockPlayer = false;
+        isShot = false;
     }
 }
