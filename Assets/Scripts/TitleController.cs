e0fe5c37e1326f43a954afbcb5d9e42728b40ec7
CanvasDEV-br <75525719+CanvasDEV-br@users.noreply.github.com>
2020-12-11 22:38:07 -0300

    Começo do pretitle

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    public Dialogo dialogo;
+    public List<string> falas;
+    public Text txt;
+
+    private string falaAtual;
+
+    private void Start() {
+        
+    }
+}
