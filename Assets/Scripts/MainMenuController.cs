53798dd9b0ad002f4e303267554df6adaafe6d5b
Vinícius Martins <vinicius.martins@fcamara.com.br>
2020-12-13 03:25:49 -0300

    Ajustes gerais

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+    public new Transform transform;
+    void Start() {
+        transform = GetComponent<Transform>();
+    }
+
+    void Update() {
+        transform.position += Vector3.right * 0.01f;
+    }
+}
