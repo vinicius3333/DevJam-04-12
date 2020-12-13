a9df433ad132d93390c0ce92320cef68b1c1baa0
Vinícius Martins <vinicius.martins@fcamara.com.br>
2020-12-13 11:56:18 -0300

    Adicionando checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    private Animator animator;
+
+    public Transform posicaoReset;
+
+    private GameMaster gm;
+
+    private bool isUsed = false;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player" && !isUsed) {
+            isUsed = true;
+            animator.SetTrigger("checkpoint");
+            gm.CheckpointPlayer(posicaoReset.position);
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,21 +8,29 @@ public class GameController : MonoBehaviour {
     public GameObject transition;
     private FadeController _FadeController;
     private bool isWait;
+
+    private GameMaster gameMaster;
+
+
     private void Start() {
         transition.SetActive(true);
         _FadeController = FindObjectOfType(typeof(FadeController)) as FadeController;
+        gameMaster = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
     }
 
     public void resetarCena() {
-        if(isWait == true) {return;}
+        if (isWait == true) { return; }
         StartCoroutine("WaitToReset");
     }
 
-   IEnumerator WaitToReset()
-    {
+    IEnumerator WaitToReset() {
         isWait = true;
         _FadeController.FadeIn();
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void proximaCena() {
+        gameMaster.posicaoPlayer = Vector2.zero;
     }
 }
